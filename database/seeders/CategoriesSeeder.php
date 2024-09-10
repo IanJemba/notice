d89e8c174ff3ede3018468f148ea558a2081f005
LeZwart <192443@novacollege.nl>
2024-09-10 10:35:50 +0200

    created model, seeder and json file for categories

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Container\Attributes\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = json_decode(file_get_contents(database_path('json/categories.json')), true);
+        foreach ($categories as $category) {
+            Category::create(
+                [
+                    'title' => $category['title'],
+                    'description' => $category['description'],
+                ]
+            );
+        }
+    }
+}
