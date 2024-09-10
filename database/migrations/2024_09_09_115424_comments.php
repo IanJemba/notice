e9f692e0a051808f9672cc5e88347fed92ecce39
Ali <alimohalghurbani@gmail.com>
2024-09-10 11:04:55 +0200

    i fixed the model and table for the comments and did some renaming

--- database/migrations/2024_09_09_115424_comments.php
+++ database/migrations/2024_09_09_115424_comments.php
@@ -11,7 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('commentaars', function (Blueprint $table) {
+            $table->id('comment_id');
+            $table->text('content');
+            $table->unsignedBigInteger('news_id');
+            $table->foreign('notice_id')->references('notice_id')->on('notices')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
