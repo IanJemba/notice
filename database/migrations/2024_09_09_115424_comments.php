000a2abd288f73e0f302c8773e9944a48cfbc0b9
Ali <alimohalghurbani@gmail.com>
2024-09-11 10:56:23 +0200

    comments migration edited

--- database/migrations/2024_09_09_115424_comments.php
+++ database/migrations/2024_09_09_115424_comments.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('commentaars', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id('comment_id');
             $table->text('content');
-            $table->unsignedBigInteger('news_id');
+            $table->unsignedBigInteger('notice_id');
             $table->foreign('notice_id')->references('notice_id')->on('notices')->onDelete('cascade');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
