a1eca534de6b351668bbb727afb556e18dedbe79
Ali <alimohalghurbani@gmail.com>
2024-09-16 15:34:18 +0200

    notice_id

--- resources/views/notices/index.blade.php
+++ resources/views/notices/index.blade.php
@@ -17,26 +17,27 @@
             @foreach ($notices as $notice)
                 <div
                     class="p-6 bg-white border border-gray-200 rounded-lg shadow-md transition-transform duration-300 transform hover:scale-105 hover:shadow-xl">
-                    <a href="{{ route('notices.show', $notice->id) }}">
+                    <a href="{{ route('notices.show', $notice->notice_id) }}">
                         <h5 class="mb-2 text-xl font-semibold text-gray-900">{{ $notice->title }}</h5>
                     </a>
                     <p class="mb-3 text-sm text-gray-700">{{ $notice->description }}</p>
                     <div class="flex items-center justify-between">
-                        <a href="{{ route('notices.show', $notice->id) }}"
+                        <a href="{{ route('notices.show', $notice->notice_id) }}"
                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Read more
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
                         </a>
                         {{-- @if (auth()->check() && auth()->user()->role == 'author' && $notice->user_id == auth()->user()->id) --}}
-                        <a href="{{ route('notices.edit', $notice->id) }}"
+                        <a href="{{ route('notices.edit', $notice->notice_id) }}"
                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-700 bg-blue-200 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300">
                             Edit
                         </a>
-                        <form action="{{ route('notices.destroy', $notice->id) }}" method="POST" class="inline-block">
+                        <form action="{{ route('notices.destroy', $notice->notice_id) }}" method="POST"
+                            class="inline-block">
                             @csrf
                             @method('DELETE')
                             <button type="submit"
@@ -52,4 +53,3 @@
         </div>
     </div>
 </x-app-layout>
-
