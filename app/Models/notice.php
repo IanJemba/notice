d49938a1120c24d1c6795958bcaf911c54ed56c4
LeZwart <192443@novacollege.nl>
2024-09-17 11:06:19 +0200

    Created relations between the models

--- app/Models/notice.php
+++ app/Models/notice.php
@@ -17,4 +17,19 @@ class notice extends Model
     ];
 
     protected $primaryKey = 'notice_id';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
