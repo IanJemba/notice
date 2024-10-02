94fa9479439ab718de69be02fbd8c6c0f23126cf
LeZwart <192443@novacollege.nl>
2024-10-02 11:14:27 +0200

    MarkingController, name ettiqute fix, routing

--- app/Http/Controllers/MarkingController.php
+++ app/Http/Controllers/MarkingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Marking;
+use Illuminate\Support\Facades\Auth;
 
 class MarkingController extends Controller
 {
@@ -15,11 +16,57 @@ class MarkingController extends Controller
 
     public function create()
     {
+        if (!Auth::user()->role->name === 'admin') {
+            return redirect()->back();
+        }
+
         return view('marking.create');
     }
 
     public function edit(Marking $marking)
     {
+        if (!Auth::user()->role->name === 'admin') {
+            return redirect()->back();
+        }
+
         return view('marking.edit', compact('marking'));
+    }
+
+    public function store(Request $request)
+    {
+        if (!Auth::user()->role->name === 'admin') {
+            return redirect()->back();
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'color' => 'required|string',
+            'disable_comments' => 'required|boolean',
+            'hide_notice' => 'required|boolean',
+        ]);
+
+        Marking::create($validatedData);
+
+        return redirect()->route('marking.index');
+    }
+
+    public function update(Request $request, Marking $marking)
+    {
+        if (!Auth::user()->role->name === 'admin') {
+            return redirect()->back();
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'color' => 'required|string',
+            'disable_comments' => 'required|boolean',
+            'hide_notice' => 'required|boolean',
+        ]);
+
+        $marking->update($validatedData);
+
+        return redirect()->route('marking.index');
     }
 }
